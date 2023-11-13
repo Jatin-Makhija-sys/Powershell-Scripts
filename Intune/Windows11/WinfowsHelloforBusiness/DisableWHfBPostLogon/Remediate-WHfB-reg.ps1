f276e70560722cd0b01da961e35174aa8fc4948d
Jatin Makhija <54775366+Jatin-Makhija-sys@users.noreply.github.com>
2023-11-13 19:51:49 +0000

    update remediate script

--- Intune/Windows11/WinfowsHelloforBusiness/DisableWHfBPostLogon/Remediate-WHfB-reg.ps1
+++ Intune/Windows11/WinfowsHelloforBusiness/DisableWHfBPostLogon/Remediate-WHfB-reg.ps1
@@ -1,12 +1,12 @@
 <# 
 .SYNOPSIS 
-Detect If WHfB registry keys and values exist
+Remediate WHfB Registry Keys and values
  
 .DESCRIPTION 
-This script will detect if WHfB registry entries are 
-created correctly to disable Post Logon WHfB prompt
+This Script will check if WHfB registry keys are created
+and set to correct values, if not, it will fix it
 .NOTES     
-        Name       : Detect-WHfB.ps1
+        Name       : Remediate-WHfB.ps1
         Author     : Jatin Makhija  
         Version    : 1.0.0  
         DateCreated: 13-Nov-2023
@@ -16,47 +16,52 @@ created correctly to disable Post Logon WHfB prompt
 https://cloudinfra.net 
 #>
 
-# Define the registry path to check
-$regPath = "HKLM:\SOFTWARE\Policies\Microsoft\PassportForWork"
 
-# Define an array of registry values to check
+# Specify the registry key path
+$regKeyPath = "HKLM:\SOFTWARE\Policies\Microsoft\PassportForWork"
+
+# Specify registry entries
 $valueNames = @("DisablePostLogonProvisioning", "Enabled")
 
-# Define the required value that the registry entries should match
+# Specify the required value
 $requiredValue = "1"
 
 # Check if the registry key exists
-$regKeyExists = Test-Path -Path $regPath
+$regKeyExists = Test-Path -Path $regKeyPath
 
-# If the registry key exists, proceed with checking registry values
 if ($regKeyExists) {
-    # Loop through each specified registry value
+    # Loop through each specified registry entry
     foreach ($valueName in $valueNames) {
         # Check if the registry entry exists
-        $regEntryExists = Get-ItemProperty -Path $regPath -Name $valueName -ErrorAction SilentlyContinue
+        $regEntryExists = Get-ItemProperty -Path $regKeyPath -Name $valueName -ErrorAction SilentlyContinue
 
-        # If the registry entry exists, fetch its value
         if ($regEntryExists) {
-            $currentValue = Get-ItemProperty -Path $regPath | Select-Object -ExpandProperty $valueName -ErrorAction SilentlyContinue
+            # If the registry entry exists, fetch its current value
+            $currentValue = $regEntryExists.$valueName
 
-            # Check if the registry value matches the required value
-            if ($currentValue -eq $requiredValue) {
-                Write-Host "Registry value '$valueName' exists and matches the required value."
-                Exit 0
+            # Check if the current value matches the required value
+            if ($currentValue -ne $requiredValue) {
+                # If the current value does not match the required value, update it
+                Set-ItemProperty -Path $regKeyPath -Name $valueName -Value $requiredValue
+                Write-Host "Registry entry '$valueName' updated to the required value '$requiredValue'"
             } else {
-                Write-Host "Registry value '$valueName' exists, but does not match the required value."
-                Write-Host "Current value: $currentValue"
-                Write-Host "Required value: $requiredValue"
-                Exit 1
+                Write-Host "Registry entry '$valueName' is already set to the required value."
             }
         } else {
-            # If the registry entry does not exist
-            Write-Host "Registry value '$valueName' does not exist."
-            Exit 1
+            # If the registry entry does not exist, create it
+            New-ItemProperty -Path $regKeyPath -Name $valueName -PropertyType DWORD -Value $requiredValue
+            Write-Host "Registry entry '$valueName' created with the required value '$requiredValue'"
         }
     }
 } else {
-    # If the registry key does not exist
-    Write-Host "Registry key does not exist."
-    Exit 1
+    # If the registry key does not exist, create it along with the specified registry entries
+    New-Item -Path $regKeyPath -Force
+    foreach ($valueName in $valueNames) {
+        New-ItemProperty -Path $regKeyPath -Name $valueName -PropertyType DWORD -Value $requiredValue
+        Write-Host "Registry key '$regKeyPath' and entry '$valueName' created with the required value '$requiredValue'"
+        Exit 0
+    }
 }
+
+Write-Host "Registry check and update completed."
+Exit 0
