4134b9fea6b5eaddfaac200183218737b40f8bfc
Jatin Makhija <54775366+Jatin-Makhija-sys@users.noreply.github.com>
2025-10-05 10:57:02 +0100

    Connect_MgGraph with certificate

---
+++ Microsoft Graph PowerShell/Connect-MgGraph_Cert.ps1
@@ -0,0 +1,16 @@
+$TenantId = "<tenantID>"
+$ClientId = "<clientID>"
+$CertThumbprint = "<certThumbprint>"
+
+$cert = Get-ChildItem Cert:\CurrentUser\My |
+  Where-Object { (($_.Thumbprint -replace '\s','') ).ToUpper() -eq $CertThumbprint.ToUpper() } |
+  Select-Object -First 1
+
+if (-not $cert) { throw "Certificate with thumbprint $CertThumbprint not found in CurrentUser\My." }
+if (-not $cert.HasPrivateKey) { throw "Found cert, but it does NOT have a private key. Install a PFX or use CurrentUser\My." }
+
+# 2) Connect using the X509Certificate2 object
+Connect-MgGraph -TenantId $TenantId -ClientId $ClientId -Certificate $cert
+
+# Optional sanity check
+(Get-MgContext) | Select TenantId, AuthType, AppName
